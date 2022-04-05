4b9c6f292ea3da0ad13ac2a51f64c662b9c0da64
solomonakp <solomonakp@gmail.com>
2022-04-05 10:50:05 +0100

    Initial Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3c34564320c106155e93c49091b1b421'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1d0e9c7a-e1da-4093-9745-eebaebc25601'
+};
