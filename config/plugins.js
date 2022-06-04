ee4ea6c02b374aade0ae9dc28af55b84980d6c4c
solomonakp <solomonakp@gmail.com>
2022-06-04 16:07:56 +0100

    fix upload issue

--- config/plugins.js
+++ config/plugins.js
@@ -1,16 +1,10 @@
 module.exports = ({ env }) => ({
   upload: {
-    config: {
-      provider: "cloudinary",
-      providerOptions: {
-        cloud_name: env("CLOUDINARY_NAME"),
-        api_key: env("CLOUDINARY_KEY"),
-        api_secret: env("CLOUDINARY_SECRET"),
-      },
-      actionOptions: {
-        upload: {},
-        delete: {},
-      },
+    provider: "cloudinary",
+    providerOptions: {
+      cloud_name: env("CLOUDINARY_NAME"),
+      api_key: env("CLOUDINARY_KEY"),
+      api_secret: env("CLOUDINARY_SECRET"),
     },
   },
 });
